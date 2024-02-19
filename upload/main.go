b3a809f043e151e93a5b0ec8812e1097f255ff7a
ernest <shiwb@shideMac-mini.local>
2024-02-19 14:46:42 +0800

    ernest: add more case

--- upload/main.go
+++ upload/main.go
@@ -13,14 +13,15 @@ import (
 )
 
 var (
-	rpcAddr = "http://127.0.0.1:26658"
+	rpcAddr  = "http://127.0.0.1:26658"
+	rpcToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJwdWJsaWMiLCJyZWFkIiwid3JpdGUiXX0.CP0e8U2NWFtvpj-krp8DFutzK0s7ycH03Nf08z5fCag"
 )
 
 func main() {
 	fmt.Println("this is a celestia upload test")
 
 	ctx := context.TODO()
-	client, err := rpc.NewClient(ctx, rpcAddr, "")
+	client, err := rpc.NewClient(ctx, rpcAddr, rpcToken)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -37,12 +38,26 @@ func main() {
 
 	ceda := celestia.NewCelestiaDA(client, namespace, -1, ctx)
 
-	msg1 := []byte("message 1")
+	msg1 := []byte("message 2")
 	//msg2 := []byte("message 2")
 
 	ctx = context.TODO()
+	//submit data
 	id1, proof1, err := ceda.Submit(ctx, []da.Blob{msg1}, -1)
-	log.Println(id1)
-	log.Println(proof1)
-	log.Println(err)
+	for i := 0; i < len(id1); i++ {
+		log.Println("id: ", hex.EncodeToString(id1[i]))
+		log.Println("proof: ", hex.EncodeToString(proof1[i]))
+	}
+	//get blob
+	blob1, err := ceda.Get(ctx, id1)
+	log.Println(string(blob1[0]))
+
+	commitment1, err := ceda.Commit(ctx, []da.Blob{msg1})
+	log.Println(hex.EncodeToString(commitment1[0]))
+
+	//verify blob
+	oks, err := ceda.Validate(ctx, id1, proof1)
+	for _, value := range oks {
+		log.Println("verify result: ", value)
+	}
 }
